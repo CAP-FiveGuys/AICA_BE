ece289337a8df36db855a61f9687675c04a76644
lozzzy <dudgns010801@icloud.com>
2025-03-26 21:22:14 +0900

    first commit

---
+++ aivoca/src/main/java/com/aica/aivoca/AivocaApplication.java
@@ -0,0 +1,13 @@
+package com.aica.aivoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AivocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AivocaApplication.class, args);
+	}
+
+}
