d621ffdfa632849e7f36809a82a9fc1630941ca4
lozzzy <dudgns010801@icloud.com>
2025-04-04 02:00:09 +0900

    :zap: [CHORE] 코드 수정

--- aivoca/src/main/java/com/aica/aivoca/global/exception/auth/SecurityConfig.java
+++ aivoca/src/main/java/com/aica/aivoca/global/exception/auth/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,14 +22,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        // 이메일 인증 관련 API는 인증 없이 접근 허용
-                        .requestMatchers(AUTH_WHITE_LIST).permitAll()
-
-                        // 나머지는 인증 필요
-                        .anyRequest().authenticated()
-                );
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(AUTH_WHITE_LIST).permitAll();
+                    auth.anyRequest().authenticated();
+                });
 
         return http.build();
     }
