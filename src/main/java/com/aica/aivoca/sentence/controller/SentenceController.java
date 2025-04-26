5f181d9fc6e00ca6786d06520d7fd59f50a3bd30
LEG4280 <dldmsrb4280@naver.com>
2025-04-26 12:06:04 +0900

    [FEAT] 문장 삭제 DELETE API 구현

--- src/main/java/com/aica/aivoca/sentence/controller/SentenceController.java
+++ src/main/java/com/aica/aivoca/sentence/controller/SentenceController.java
@@ -5,6 +5,7 @@ import com.aica.aivoca.sentence.dto.SentenceGetResponseDto;
 import com.aica.aivoca.sentence.dto.SentenceRequestDto;
 import com.aica.aivoca.sentence.dto.SentenceResponseDto;
 import com.aica.aivoca.sentence.service.addSentenceService;
+import com.aica.aivoca.sentence.service.deleteSentenceService;
 import com.aica.aivoca.sentence.service.getSentenceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ public class SentenceController {
 
     private final addSentenceService addSentenceService;
     private final getSentenceService getSentenceService;
+    private final deleteSentenceService deleteSentenceService;
 
     @PostMapping
     public SuccessStatusResponse<SentenceResponseDto> addSentence(@RequestBody SentenceRequestDto requestDto) {
@@ -30,6 +32,12 @@ public class SentenceController {
             @RequestParam(value = "search", required = false) String search
     ) {
         return getSentenceService.getSentences(null, search); // userId는 무시됨
+    }
+
+    @DeleteMapping("/{id}") // HTTP DELETE 메서드 매핑
+    public SuccessStatusResponse<Void> deleteSentence(@PathVariable Long id) {
+        // Controller에서는 사용자 ID를 직접 처리하지 않고 Service에 위임
+        return deleteSentenceService.deleteSentence(id);
     }
 
 }
