1d267b7c950de257391360d1256b2d6efd1da899
hyosub <gytjqdlekdy@naver.com>
2025-05-29 10:28:25 +0900

    [FEAT] 로그아웃 기능 구현(리프레시 토큰 삭제)

---
+++ src/main/java/com/aica/aivoca/logout/controller/LogoutController.java
@@ -0,0 +1,27 @@
+package com.aica.aivoca.logout.controller;
+
+import com.aica.aivoca.global.exception.dto.SuccessStatusResponse;
+import com.aica.aivoca.global.exception.message.SuccessMessage;
+import com.aica.aivoca.logout.service.LogoutService;
+import com.aica.aivoca.global.jwt.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class LogoutController {
+
+    private final LogoutService logoutService;
+
+    @PostMapping("/logout")
+    public ResponseEntity<SuccessStatusResponse<Void>> logout(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestHeader(value = "Authorization", required = false) String authHeader) {
+
+        logoutService.logout(userDetails.userId(), authHeader);
+        return ResponseEntity.ok(SuccessStatusResponse.of(SuccessMessage.USER_LOGOUT_SUCCESS));
+    }
+}
